591510dffb6a7b9a452e29ced2c6e91b8d9b564f
ak0982 <51588443+ak0982@users.noreply.github.com>
2020-02-09 23:47:00 +0530

    Update 8queen_by_a_star.cpp

--- 8queen_by_a_star.cpp
+++ 8queen_by_a_star.cpp
@@ -1,3 +1,19 @@
+/*
+name amar-kumar
+roll no -1801016
+
+
+1.State space:
+       Here sate space is a 8x8 matrix where the position of a queen is represented by 1
+2.State transition operator:
+		adding a new queen which is denoted by 1 in the subsequent row.
+3.intitial state:
+		a 8 x 8 null matrix.
+4.Goal state:
+		When all the rows are filled with a single queen at non attacking positions,
+
+
+*/
 #include<iostream>
 #include<vector>
 #include<algorithm>
